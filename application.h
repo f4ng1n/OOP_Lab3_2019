77042860b97367673b369724b22e006d461a8dfb
Phan Thị Hải Hà <phanha.leti@gmail.com>
2019-04-02 04:28:48 +0300

    Create application.h

---
+++ application.h
@@ -0,0 +1,12 @@
+#ifndef TAPPLICATION_H
+#define TAPPLICATION_H
+
+#include <QCoreApplication>
+class TApplication : public QCoreApplication
+{
+public:
+    TApplication(int, char **);
+    int run();
+};
+
+#endif // TAPPLICATION_H
